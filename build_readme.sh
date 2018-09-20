be9afa88063ae5d712929cd03f2fa3144a02c2a4
sudorook <daemon@nullcodon.com>
2018-09-20 13:16:16 -0500

    switch build_readme.sh from 3 to 2 space indentation

--- build_readme.sh
+++ build_readme.sh
@@ -44,44 +44,44 @@ PLUGIN_NAME=$(cat "${CPPFILE}" | \
               sed -n "s/.*DefaultGUIModel(\"\(.*\)\",.*/\1/p" | \
               sed -e 's/ \([A-Z][a-z]\)/ \1/g')
 DESCRIPTION=$(awk '/^[ \t]*setWhatsThis.*/,/\);/' "${CPPFILE}" | \
-             tr -d '\n' | sed -n "s/^\s*setWhatsThis(.*\"\(.*\)\");/\1\n/p")
+              tr -d '\n' | sed -n "s/^\s*setWhatsThis(.*\"\(.*\)\");/\1\n/p")
 VARS_ARRAY=$(grep -Pzo "(?s)(\s*)\N*vars\[\].*?{.*?\1};" "${CPPFILE}")
 CATAPULT=$(echo ${VARS_ARRAY} | tr -d '\n' | \
 awk ' BEGIN { p=0 } 
 {
-   for(i=1; i<=length($0); i++)
-   {
-      test=substr($0, i, 1)
-      if(test=="{") {
-         p=1
-         continue
-      }
-      if(test=="}") {
-         printf("\n")
-         p=0;
-      }
-      if(p==1 && test!="\t") {
-         printf("%s", test)
-      }
-   }
+  for(i=1; i<=length($0); i++)
+  {
+    test=substr($0, i, 1)
+    if(test=="{") {
+      p=1
+      continue
+    }
+    if(test=="}") {
+      printf("\n")
+      p=0;
+    }
+    if(p==1 && test!="\t") {
+      printf("%s", test)
+    }
+  }
 } ')
 
 function PRINT_VARIABLES() {
-   LIST=$1
-   TYPE=$2
-   COUNTER=1
-   while read -r LINE; do
-      if [[ "$LINE" =~ "$TYPE" ]]; then
-         NAME=$(echo ${LINE} | cut -d "," -f1 | sed "s/^[ \t]*//" | sed -e "s/\"//g")
-         DESCRIPTION=$(echo ${LINE} | cut -d "," -f2 | sed "s/^[ \t]*//" | sed -e "s/\"//g")
-         if [[ "$TYPE" == INPUT ]] || [[ "$TYPE" == OUTPUT ]]; then
-            echo "$COUNTER. ${TYPE,,}($((COUNTER-1))) - $NAME : $DESCRIPTION"
-         else
-            echo "$COUNTER. $NAME - $DESCRIPTION"
-         fi
-         COUNTER=$((COUNTER+1))
+  LIST=$1
+  TYPE=$2
+  COUNTER=1
+  while read -r LINE; do
+    if [[ "$LINE" =~ "$TYPE" ]]; then
+      NAME=$(echo ${LINE} | cut -d "," -f1 | sed "s/^[ \t]*//" | sed -e "s/\"//g")
+      DESCRIPTION=$(echo ${LINE} | cut -d "," -f2 | sed "s/^[ \t]*//" | sed -e "s/\"//g")
+      if [[ "$TYPE" == INPUT ]] || [[ "$TYPE" == OUTPUT ]]; then
+        echo "$COUNTER. ${TYPE,,}($((COUNTER-1))) - $NAME : $DESCRIPTION"
+      else
+        echo "$COUNTER. $NAME - $DESCRIPTION"
       fi
-   done <<< "${LIST}"
+      COUNTER=$((COUNTER+1))
+    fi
+  done <<< "${LIST}"
 }
 
 ################################################################################
